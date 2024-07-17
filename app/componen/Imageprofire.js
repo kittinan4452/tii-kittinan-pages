84a7e05d59ece1eb3bf6e6e7b8a0ec06a37aca96
kittinan4452 <kittinan_ku@kkumail.com>
2024-07-17 17:10:53 +0700

    addcode

--- app/componen/Imageprofire.js
+++ app/componen/Imageprofire.js
@@ -3,7 +3,7 @@ import Image from "next/image"
 export default function Imageprofile(){
     return(
         <div className="lg:ml-32 mask mask-hexagon-2 shadow-2xl "  >
-            <Image src="/image/tii.jpg" width={400} height={300}  className="p-12 mask mask-hexagon-2  shadow-inner "/>
+            <Image src="./image/tii.jpg" width={400} height={300}  className="p-12 mask mask-hexagon-2  shadow-inner "/>
         </div>
     )
 }
