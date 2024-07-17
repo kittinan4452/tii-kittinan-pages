cb8a86faa76be3f635537982c860f447e75d4023
kittinan4452 <kittinan_ku@kkumail.com>
2024-07-17 16:45:06 +0700

    pushcode

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: "/tii-kittinan-pages",
     output: "export",
+    reactStrictMode: true,
 };
 
 export default nextConfig;
