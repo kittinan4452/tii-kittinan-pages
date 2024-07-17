171f31960a46dbc61f39bcba9b8a0e7ca40ea7e6
kittinan4452 <kittinan_ku@kkumail.com>
2024-07-17 17:27:04 +0700

    addcode3

--- app/componen/Imageprofire.js
+++ app/componen/Imageprofire.js
@@ -1,9 +1,10 @@
 "use client";
 import Image from "next/image"
+const assetPrefix = '/tii-kittinan-pages';
 export default function Imageprofile(){
     return(
         <div className="lg:ml-32 mask mask-hexagon-2 shadow-2xl "  >
-            <Image src="../image/tii.jpg" width={400} height={300}  className="p-12 mask mask-hexagon-2  shadow-inner "/>
+            <Image src="/image/tii.jpg" width={400} height={300}  className="p-12 mask mask-hexagon-2  shadow-inner "/>
         </div>
     )
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: "/tii-kittinan-pages",
-    assetPrefix: "/tii-kittinan-pages/",
+    assetPrefix: "/tii-kittinan-pages",
     trailingSlash: true,  // เพิ่ม trailingSlash เพื่อให้ URL มีเครื่องหมาย / ต่อท้าย
     reactStrictMode: true,
     output: "export",
