afc754f44c4c57ea3076778cf90f6839963e6a59
kittinan4452 <kittinan_ku@kkumail.com>
2024-07-17 17:32:08 +0700

    addcode4

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: "/tii-kittinan-pages",
-    assetPrefix: "/tii-kittinan-pages",
-    trailingSlash: true,  // เพิ่ม trailingSlash เพื่อให้ URL มีเครื่องหมาย / ต่อท้าย
-    reactStrictMode: true,
     output: "export",
+    images: {
+        unoptimized: true,
+      },
   };
   
   export default nextConfig;
