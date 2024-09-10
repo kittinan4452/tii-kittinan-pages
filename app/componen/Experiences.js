ec1f1d40d9855da9b49f69d0bf46e136f832b643
kittinan4452 <kittinan_ku@kkumail.com>
2024-09-10 15:42:49 +0700

    addnewdatalast

--- app/componen/Experiences.js
+++ app/componen/Experiences.js
@@ -288,7 +288,6 @@ export default function Experiences(){
 <dialog id="my_modal_6" className="modal  ">
   <div className="modal-box w-11/12 max-w-5xl bg-[#1D232A] text-white ">
     <form method="dialog">
-       if there is a button in form, it will close the modal 
       <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
     </form>
     <h1 className="font-bold text-2xl text-center text-red-700">Porject-Test-Front-End</h1>
